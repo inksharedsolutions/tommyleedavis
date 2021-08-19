7d41f55f3f4956a3c50a8d07a381938ea1daefec
inksharedsolutions <57968903+inksharedsolutions@users.noreply.github.com>
2021-08-19 08:10:15 +0800

    Update book3Links.js

--- src/components/Links/book3Links.js
+++ src/components/Links/book3Links.js
@@ -16,7 +16,7 @@ const Links = () =>{
 				<span>Paperback :</span>
 				<li><a rel="noopener noreferrer" target="_blank" href="https://www.amazon.com/Asims-Extraordinary-Journeys-Crystal-Chalice/dp/1648954782/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1628836484&sr=8-1">Amazon</a></li>
 				<li><a rel="noopener noreferrer" target="_blank" href="https://www.barnesandnoble.com/w/asims-extraordinary-journeys-tommy-lee-davis/1139976651?ean=9781648954788">Barnes & Noble</a></li>
-				{/* <li><a rel="noopener noreferrer" target="_blank" href="https://www.booksamillion.com/p/God-Answered-Tough-Times/Carl-Moore/9781643457284?id=8049069540563&_ga=2.136780400.1998066025.1605877581-2145380449.1604906972">Books A Million</a></li> */}
+				<li><a rel="noopener noreferrer" target="_blank" href="https://www.booksamillion.com/p/Asims-Extraordinary-Journeys/Tommy-Lee-Davis/9781648954788?id=8270570794427&__cf_chl_jschl_tk__=pmd_2b24963948e4864f2c7860cf7fa1596525e8129e-1629331608-0-gqNtZGzNAnijcnBszQrO">Books A Million</a></li>
 			</nav>	
 		</>	
 	)
